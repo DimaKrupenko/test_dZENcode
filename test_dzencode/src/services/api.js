c8ca0dcf82e601f3d24080c76cfba99ec875c02d
DimaKrupenko <dimakrupenko77@gmail.com>
2023-03-14 15:09:35 +0200

    upd

--- test_dzencode/src/services/api.js
+++ test_dzencode/src/services/api.js
@@ -4,7 +4,6 @@ const API = {
   async get() {
     const url = 'http://localhost:3001/api/comments/';
     const response = await axios.get(url);
-    // console.log(response.data.data.result);
     return response.data.data.result;
   },
 };
