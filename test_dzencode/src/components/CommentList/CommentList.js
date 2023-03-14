a142b2fc8206bb6f9c981a28d2253022d5706b3e
DimaKrupenko <dimakrupenko77@gmail.com>
2023-03-14 15:37:49 +0200

    upd

--- test_dzencode/src/components/CommentList/CommentList.js
+++ test_dzencode/src/components/CommentList/CommentList.js
@@ -4,21 +4,21 @@ import styles from './CommentList.module.css';
 const CommentList = ({ comments }) => {
   return (
     <ul>
-      {comments.map(comment => (
-        <li key={comment._id} className={styles.list}>
+      {comments.map(({ _id, username, email, homepage, text }) => (
+        <li key={_id} className={styles.list}>
           <div className={styles.userNameConteiner}>
-            <h2 className={styles.userName}>{comment.username}</h2>
-            <p className={styles.email}>{comment.email}</p>
+            <h2 className={styles.userName}>{username}</h2>
+            <p className={styles.email}>{email}</p>
           </div>
-          {comment.homepage && (
+          {homepage && (
             <div>
               Home page:
-              <a className={styles.homePage} href={comment.homepage}>
-                {comment.homepage}
+              <a className={styles.homePage} href={homepage}>
+                {homepage}
               </a>
             </div>
           )}
-          <p>{comment.text}</p>
+          <p>{text}</p>
         </li>
       ))}
     </ul>
